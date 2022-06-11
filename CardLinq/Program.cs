588b0a7238cc21cfa367e6e956e4801624a8af66
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-11 18:37:27 +0200

    used group...by linq query

--- CardLinq/Program.cs
+++ CardLinq/Program.cs
@@ -6,6 +6,21 @@ namespace CardLinq
     {
         static void Main(string[] args)
         {
+            var deck = new Deck().Shuffle().Take(16);
+
+            var grouped =
+                from card in deck
+                group card by card.Suit into suitGroup
+                orderby suitGroup.Key descending
+                select suitGroup;
+
+            foreach ( var group in grouped)
+            {
+                Console.WriteLine(@$"Grupa: {group.Key}
+Liczba elementów: {group.Count()}
+Minimum: {group.Min()}
+Maksimum: {group.Max()}");
+            }
             
         }
     }
